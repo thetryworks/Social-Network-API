0c717720261ea0bef66080747398db569ed572e0
thetryworks <natesmyth1@gmail.com>
2022-06-15 10:51:08 -0500

    finished thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../..controllers/thought-controller');
+
+router.route('/:userId').post(createThought);
+
+router
+    .route('/:userId/:thoughtId')
+    .put(createReaction)
+    .delete(deleteThought)
+
+router.route('/:userId/:thoughtId/:reactionId').delete(deleteReaction);
+
+
 module.exports = router;
